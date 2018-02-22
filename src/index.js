5f09a8a67a336b7010fa189e53c8ccdf766f5a05
AmsterGet <amsterget@gmail.com>
2018-02-22 16:37:00 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    const symbolsMas = equation.split(' ');
+    const rezMas = [];
+    const a = +symbolsMas[0];
+    const b = +(symbolsMas[3]+symbolsMas[4]);
+    const c = +(symbolsMas[7]+symbolsMas[8]);
+    const d = b*b-4*a*c;
+    const x1 = Math.round((-b+Math.sqrt(d))/(2*a));
+    const x2 = Math.round((-b-Math.sqrt(d))/(2*a));
+    rezMas.push(x1);
+    x1>x2 ? rezMas.unshift(x2): rezMas.push(x2);
+    return rezMas;
+};
